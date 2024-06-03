b60c2bbecc24c0bea59c23a21bbb9bfe5fcd88f3
Kevin Maik Jablonka <mail@kjablonka.com>
2024-06-03 19:19:09 +0200

    implement fitting

---
+++ src/photolooper/fit.py
@@ -0,0 +1,158 @@
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import least_squares
+
+
+def find_nearest(array, values):
+    """
+    Find the indices of the nearest values in the array to a list of target values.
+
+    Parameters:
+        array (numpy.ndarray): The input array.
+        values (numpy.ndarray or scalar): The target values.
+
+    Returns:
+        list: A list of indices of the nearest values in the array to the target values.
+
+    Note:
+        If the input array has more than one dimension, the function flattens the first dimension.
+        The function uses the `numpy.searchsorted` function to find the indices of the target values in the array.
+        The function then checks if the index is not the last index in the array and if the difference between the target value and the previous value in the array is less than the difference between the target value and the current value in the array. If both conditions are true, the index of the previous value is returned, otherwise the index of the current value is returned.
+    """
+
+    if array.ndim != 1:
+        array_1d = array[:, 0]
+    else:
+        array_1d = array
+
+    values = np.atleast_1d(values)
+    hits = []
+
+    for i in range(len(values)):
+        idx = np.searchsorted(array_1d, values[i], side="left")
+        if idx > 0 and (
+            idx == len(array_1d)
+            or math.fabs(values[i] - array_1d[idx - 1])
+            < math.fabs(values[i] - array_1d[idx])
+        ):
+            hits.append(idx - 1)
+        else:
+            hits.append(idx)
+
+    return hits
+
+
+def poly(a, x):
+    y = a[0] * x**0
+
+    for i in range(1, len(a)):
+        y += a[i] * x**i
+
+    return y
+
+
+def residual_generic(p, x, y, function):
+    y_fit = function(p, x)
+    res = y - y_fit
+
+    return res
+
+
+def pre_signal_fitting(data, start, end, order_poly, plotting=False, filename=None):
+    """Pre-signal fitting for baseline correction"""
+
+    idx = find_nearest(data, np.array([start, end]))
+    x = data[:, 0][idx[0] : idx[1]]
+    y = data[:, 1][idx[0] : idx[1]]
+
+    p_guess = np.ones(order_poly + 1)
+    p = least_squares(fun=residual_generic, x0=p_guess, args=(x, y, poly))
+    p_solved = p.x
+
+    y_baseline = poly(p_solved, data[:, 0])
+    y_corrected = data[:, 1] - y_baseline
+
+    if plotting:
+        fig, ax = plt.subplots()
+        ax.plot(x, y, ".", markersize=2.0)
+        ax.plot(data[:, 0], y_baseline, linewidth=1.0)
+        ax.plot(data[:, 0], y_corrected)
+        if filename is not None:
+            fig.savefig(filename, dpi=400)
+
+    data_corr = np.c_[data[:, 0], y_corrected]
+
+    return data_corr
+
+
+def first_order_combined(p, t):
+    return p[0] - (p[0] * np.exp(-p[1] * t))
+
+
+def first_order_shift(p, t):
+    """Function with value zero for t < p[2], first order for t > p[2]"""
+
+    idx = find_nearest(t, p[2])[0]
+
+    t_base = t[: idx + 1]
+    t_feature = t[idx + 1 :]
+
+    y_base = t_base * 0
+    y_feature = first_order_combined(p[:2], (t_feature - p[2]))
+
+    return np.r_[y_base, y_feature]
+
+
+def first_order_fitting_without_normalization(
+    p_guess, data, plotting=False, filename=None
+):
+    """Fitting first_order_shift function to data, which has not been normalized"""
+
+    x = data[:, 0]
+    y = data[:, 1]
+
+    p = least_squares(fun=residual_generic, x0=p_guess, args=(x, y, first_order_shift))
+    y_fit = first_order_shift(p.x, x)
+
+    if plotting:
+        fig, ax = plt.subplots()
+        ax.plot(x, y, ".")
+        ax.plot(x, y_fit, linewidth=1)
+        if filename is not None:
+            fig.savefig(filename, dpi=400)
+
+    return p.x
+
+
+def fit_data(data_df, filename=None):
+    """Fit data to first order rate law"""
+
+    # subset data to relevant statuses
+    relevant_statuses = ["DEGASSING", "PREREATION-BASELINE", "REACTION"]
+    data_subset = data_df[data_df["status"].isin(relevant_statuses)]
+
+    # get relevant time and oxygen data
+    time_data = data_subset["duration"].values
+    oxygen_data = data_subset["uM_1"].values
+
+    # get pre-reaction time range
+    reaction_start_time = data_subset[data_subset["status"] == "REACTION"][
+        "duration"
+    ].values[0]
+    pre_reaction_time_range = (data_subset["duration"].values[0], reaction_start_time)
+
+    # correct baseline
+    corrected_data = pre_signal_fitting(
+        np.c_[time_data, oxygen_data], *pre_reaction_time_range, 2, plotting=False
+    )
+
+    # fit first order rate law
+    rate_law_guess = np.array([10.0, 0.01, reaction_start_time])
+    rate_law_fit = first_order_fitting_without_normalization(
+        rate_law_guess, corrected_data, plotting=True, filename=filename
+    )
+
+    # print rate constant
+    print(f"Rate constant k1 (s^-1): {rate_law_fit[1]:.3f}")
--- src/photolooper/main.py
+++ src/photolooper/main.py
@@ -1,15 +1,18 @@
+import os
 import time
-from photolooper.powersupply import switch_off, switch_on
-from photolooper.firesting import measure_firesting
-from photolooper.utils import find_com_port
-import pandas as pd
+import warnings
 from pathlib import Path
 from typing import Union
-import os
-import yaml
-from photolooper.status import Command, Status
-import warnings
+
 import matplotlib.pyplot as plt
+import pandas as pd
+import yaml
+
+from photolooper.firesting import measure_firesting
+from photolooper.powersupply import switch_off, switch_on
+from photolooper.status import Command, Status
+from photolooper.utils import find_com_port
+from photolooper.fit import fit_data
 
 warnings.filterwarnings(
     "ignore", message=".*baud*."
@@ -79,7 +82,7 @@ def obtain_command(working_directory: Union[str, Path] = "."):
     if "FIRESTING-END" in content:
         return Command.firesting_end
 
-    if "PAUSE" in content: 
+    if "PAUSE" in content:
         return Command.pause
 
     return Command.other
@@ -119,9 +122,11 @@ def write_break_command(working_directory):
     with open(os.path.join(working_directory, "command.csv"), "w") as handle:
         handle.write("PAUSE")
 
+
 def write_pause_status(working_directory):
     with open(os.path.join(working_directory, "firesting_status.csv"), "w") as handle:
         handle.write("PAUSE")
+
 
 def read_yaml(yaml_file: Union[str, Path]) -> list:
     with open(yaml_file, "r") as handle:
@@ -176,21 +181,20 @@ def main(global_config_path, experiment_config_path):
 
         # if the results file already exists, warn user and add incrementing number to run name
         if os.path.exists(
-            os.path.join(
-                global_configs["log_dir"], f"results_{config['name']}.csv"
-            )
+            os.path.join(global_configs["log_dir"], f"results_{config['name']}.csv")
         ):
             print(
                 f"🚧 Results file for {config['name']} already exists. Adding number to run name"
             )
-            config["name"] = config["name"] + "_" + str(
-                len(os.listdir(global_configs["log_dir"])) + 1
+            config["name"] = (
+                config["name"]
+                + "_"
+                + str(len(os.listdir(global_configs["log_dir"])) + 1)
             )
 
         write_instruction_csv(config, global_configs["instruction_dir"])
         results = []
         switch_off(global_configs["lamp_port"]["port"])
-        steps = 0
         while True and config["run"] == "true":
             command = obtain_command(
                 working_directory=global_configs["chemspeed_working_dir"]
@@ -201,8 +205,8 @@ def main(global_config_path, experiment_config_path):
 
             if command == Command.firesting_end:
                 # if the autosuite waits and the python code continues running and reading the firesting_end command, it will continue breaking the executions
-                write_break_command( global_configs["instruction_dir"])
-                write_pause_status( global_configs["instruction_dir"])
+                write_break_command(global_configs["instruction_dir"])
+                write_pause_status(global_configs["instruction_dir"])
                 break
 
             if command == Command.lamp_off:
@@ -211,14 +215,15 @@ def main(global_config_path, experiment_config_path):
             if command == Command.lamp_on:
                 switch_on(global_configs["lamp_port"]["port"], config["voltage"])
 
-            if command not in set([Command.firesting_stop, Command.firesting_end, Command.pause]):
+            if command not in set(
+                [Command.firesting_stop, Command.firesting_end, Command.pause]
+            ):
                 firesting_results = measure_firesting(
                     global_configs["firesting_port"]["port"]
                 )
                 print(
                     f"uO2: {firesting_results['uM_1']} optical temperature: {firesting_results['optical_temperature_2']}"
                 )
-               
 
             else:
                 firesting_results = {}
@@ -229,32 +234,32 @@ def main(global_config_path, experiment_config_path):
             firesting_results["command"] = command.value
             results.append(firesting_results)
 
-     
-
             df = pd.DataFrame(results)
-         
-            df['duration'] = df['timestamp'] - df['timestamp'].iloc[0]
+
+            df["duration"] = df["timestamp"] - df["timestamp"].iloc[0]
             if len(df) > 1:
-                df['switch'] = [False, False] + [df['status'].values[i-1] != df['status'].values[i] for i in range(1,len(df)-1)]
-                switch_times = df[df['switch']]['duration']
-            else: 
+                df["switch"] = [False, False] + [
+                    df["status"].values[i - 1] != df["status"].values[i]
+                    for i in range(1, len(df) - 1)
+                ]
+                switch_times = df[df["switch"]]["duration"]
+            else:
                 switch_times = []
             # df['duration'] = df['duration'].astype('timedelta64[min]')
-            if  command not in set([Command.firesting_stop, Command.firesting_end, Command.pause]):
+            if command not in set(
+                [Command.firesting_stop, Command.firesting_end, Command.pause]
+            ):
                 fig, ax = plt.subplots(2, 1)
-                ax[0].plot(df["duration"], df["uM_1"], '-o')
-                ax[1].plot(
-                    df["duration"],
-                    df["optical_temperature_2"],  '-o'
-                )
+                ax[0].plot(df["duration"], df["uM_1"], "-o")
+                ax[1].plot(df["duration"], df["optical_temperature_2"], "-o")
 
                 for switch_time in switch_times:
                     ax[0].axvline(switch_time)
                     ax[1].axvline(switch_time)
 
-                ax[1].set_xlabel('time / s')
-                ax[0].set_ylabel('O2 / uM')
-                ax[1].set_ylabel('T / C')
+                ax[1].set_xlabel("time / s")
+                ax[0].set_ylabel("O2 / uM")
+                ax[1].set_ylabel("T / C")
                 fig.tight_layout()
                 fig.savefig(
                     os.path.join(
@@ -278,9 +283,15 @@ def main(global_config_path, experiment_config_path):
                     ),
                     index=False,
                 )
-            except KeyError as e: # not measured yet
+            except KeyError:  # not measured yet
                 pass
 
+            fit_data(
+                df,
+                os.path.join(
+                    global_configs["log_dir"],
+                    f"fit_{config['name']}.png",
+                ),
+            )
+
             time.sleep(global_configs["sleep_time"])
-
-
